1cf561484d07a93a5f3a8c6766164b21c11dd2d9
Steven <steven.buss@gmail.com>
2012-03-04 15:28:26 -0800

    Added bash_profile and updated setup script to remove old files.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+rm $HOME/.vimrc
+rm $HOME/.bashrc
+rm $HOME/.mybashrc
+rm $HOME/.bash_profile
+rm $HOME/.gitglobalignore
 ln -s `pwd`/vimrc $HOME/.vimrc
 ln -s `pwd`/bashrc $HOME/.bashrc
 ln -s `pwd`/mybashrc $HOME/.mybashrc
-ln -s `pwd`/bashrc $HOME/.bash_profile
+ln -s `pwd`/bash_profile $HOME/.bash_profile
 ln -s `pwd`/gitglobalignore $HOME/.gitglobalignore
 git config --global core.excludesfile $HOME/.gitglobalignore
