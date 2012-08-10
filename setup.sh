518e3c98473adf5719523fa6be516a28d344906e
Steven Buss <sbuss@counsyl.com>
2012-08-09 18:12:27 -0700

    Add screenrc

--- setup.sh
+++ setup.sh
@@ -2,11 +2,13 @@
 rm $HOME/.vimrc
 rm $HOME/.bashrc
 rm $HOME/.mybashrc
+rm $HOME/.screenrc
 rm $HOME/.bash_profile
 rm $HOME/.gitglobalignore
 ln -s `pwd`/vimrc $HOME/.vimrc
 ln -s `pwd`/bashrc $HOME/.bashrc
 ln -s `pwd`/mybashrc $HOME/.mybashrc
+ln -s `pwd`/screenrc $HOME/.screenrc
 ln -s `pwd`/bash_profile $HOME/.bash_profile
 ln -s `pwd`/gitglobalignore $HOME/.gitglobalignore
 git config --global core.excludesfile $HOME/.gitglobalignore
