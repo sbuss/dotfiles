b99170f375ded09f69cefc81e09d50d463cf7d16
Steven Buss <steven.bus@gmail.com>
2015-11-14 14:28:29 -0800

    Copy some xmonad conf from jamesob

--- setup.sh
+++ setup.sh
@@ -1,25 +1,64 @@
 #!/bin/bash
-rm $HOME/.vimrc
-rm $HOME/.vim/plugins
-rm $HOME/.bashrc
-rm $HOME/.mybashrc
-rm $HOME/.screenrc
-rm $HOME/.bash_profile
-rm $HOME/.gitglobalignore
-mkdir -p $HOME/.vim
-mkdir -p $HOME/.vim/colors
-ln -s `pwd`/vimrc $HOME/.vimrc
-ln -s `pwd`/vimrc_plugins $HOME/.vim/plugins
-ln -s `pwd`/my-vim-colors/*.vim $HOME/.vim/colors/
-ln -s `pwd`/bashrc $HOME/.bashrc
-ln -s `pwd`/mybashrc $HOME/.mybashrc
-ln -s `pwd`/screenrc $HOME/.screenrc
-ln -s `pwd`/bash_profile $HOME/.bash_profile
-ln -s `pwd`/gitglobalignore $HOME/.gitglobalignore
-git config --global core.excludesfile $HOME/.gitglobalignore
 
-rm -rf $HOME/.vim/bundle
-mkdir -p $HOME/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -u ~/.vim/plugins +PluginInstall +qall
-mkdir -p $HOME/.vim/swap
+OSX_UNAME='Darwin'
+LINUX_UNAME='Linux'
+
+_vim() {
+    rm $HOME/.vimrc
+    ln -s `pwd`/vimrc $HOME/.vimrc
+
+    rm $HOME/.vim/plugins
+    mkdir -p $HOME/.vim
+    mkdir -p $HOME/.vim/colors
+    ln -s `pwd`/vimrc_plugins $HOME/.vim/plugins
+    ln -s `pwd`/my-vim-colors/*.vim $HOME/.vim/colors/
+
+    rm -rf $HOME/.vim/bundle
+    mkdir -p $HOME/.vim/bundle
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim -u ~/.vim/plugins +PluginInstall +qall
+    mkdir -p $HOME/.vim/swap
+}
+
+_bash() {
+    rm $HOME/.bashrc
+    rm $HOME/.mybashrc
+    ln -s `pwd`/bashrc $HOME/.bashrc
+    ln -s `pwd`/mybashrc $HOME/.mybashrc
+    case `uname` in
+      $OSX_UNAME)
+        rm $HOME/.bash_profile
+        ln -s `pwd`/bash_profile $HOME/.bash_profile
+        ;;
+    esac
+}
+
+_git() {
+    rm $HOME/.gitglobalignore
+    ln -s `pwd`/gitglobalignore $HOME/.gitglobalignore
+    git config --global core.excludesfile $HOME/.gitglobalignore
+}
+
+_xmonad() {
+    # Xmonad
+    rm $HOME/.conkyrc
+    rm $HOME/.Xdefaults
+    ln -s `pwd`/xmonad $HOME/.xmonad
+    ln -s `pwd`/dzen $HOME/.dzen
+    ln -s `pwd`/conkyrc $HOME/.conkyrc
+    ln -s `pwd`/Xdefaults $HOME/.Xdefaults
+}
+
+all_platforms() {
+    _vim
+    _bash
+    rm $HOME/.screenrc
+    ln -s `pwd`/screenrc $HOME/.screenrc
+    case `uname` in
+      $LINUX_UNAME)
+        _xmonad
+      ;;
+    esac
+}
+
+all_platforms
