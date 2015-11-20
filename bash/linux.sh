3161b4ee9a4c8eab8b291b4a24bdcc5669ea8bcd
Steven Buss <steven.bus@gmail.com>
2015-11-20 11:29:30 -0800

    Don't set DOCKER_HOST or DOCKER_TLS in linux

--- bash/linux.sh
+++ bash/linux.sh
@@ -1,7 +1,7 @@
 # Connect docker client to boot2docker VM
 #export DOCKER_HOST=tcp://
-export DOCKER_HOST=tcp://0.0.0.0:2376
-export DOCKER_TLS_VERIFY=1
+#export DOCKER_HOST=tcp://localhost:4243
+#export DOCKER_TLS_VERIFY=1
 
 # enable bash completion in interactive shells
 . /etc/bash_completion
