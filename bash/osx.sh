47bc52dba145895f484d3628aa3ac6308b46e3b4
Steven Bacio <steven.buss@gmail.com>
2026-04-04 11:47:29 -0700

    ghostty themes

--- bash/osx.sh
+++ bash/osx.sh
@@ -39,3 +39,6 @@ export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/libpq/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/libpq/include"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
+
+# ghostty
+export PATH="$PATH:/Applications/Ghostty.app/Contents/MacOS"
