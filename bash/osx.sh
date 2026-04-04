f14dcbf68d6918158417e136b0e55880c825df6a
Steven Bacio <steven.buss@gmail.com>
2026-04-04 11:47:14 -0700

    libpq for some reason

--- bash/osx.sh
+++ bash/osx.sh
@@ -34,3 +34,8 @@ source /opt/homebrew/opt/chruby/share/chruby/auto.sh
 chruby ruby-3.2.1
 
 
+# libpq
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/libpq/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/libpq/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
